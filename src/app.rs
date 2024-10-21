0c28ce785259d4714776d534b7b7c6a1b219a15b
ragnarlodbrok1992 <maraus92@gmail.com>
2024-10-21 23:46:36 +0200

    WIP

--- src/app.rs
+++ src/app.rs
@@ -1,13 +1,24 @@
+use egui::util::cache::{ComputerMut, FrameCache};
+
 /// We derive Deserialize/Serialize so we can persist app state on shutdown.
 #[derive(serde::Deserialize, serde::Serialize)]
 #[serde(default)] // if we add new fields, give them default values when deserializing old state
 pub struct Uninterruptible {
     // Example stuff:
-    label: String,
+    // label: String,
 
-    #[serde(skip)] // This how you opt-out of serialization of a field
-    value: f32,
+    // TODO put stuff here for global context passable state
+    // this might not be it tho...
+    // command_buffer: String,
+
+    // #[serde(skip)] // This how you opt-out of serialization of a field
+    // value: f32,
 }
+
+// Memory for application - persistent, between frames etc
+// --> https://docs.rs/egui/latest/egui/struct.Memory.html#structfield.data
+// Holy shit, this is absurd...
+
 
 impl Uninterruptible {
     /// Called once before the first frame.
@@ -36,6 +47,16 @@ impl eframe::App for Uninterruptible {
         // Put your widgets into a `SidePanel`, `TopBottomPanel`, `CentralPanel`, `Window` or `Area`.
         // For inspiration and more examples, go to https://emilk.github.io/egui
         // TODO put widgets here
+        let mut hello_world = String::new();
+
+        // Running every frame application loop
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("Uninterruptible - focus work application");
+
+            // TODO put text field that inputs commands for application
+            // TODO check how to storage strings? show I use context?
+            ui.text_edit_singleline(&mut hello_world);
+        });
     }
 }
 
@@ -43,26 +64,10 @@ impl Default for Uninterruptible {
     fn default() -> Self {
         Self {
             // Example stuff:
-            label: "Hello World!".to_owned(),
-            value: 2.7,
+            // label: "Hello World!".to_owned(),
+            // value: 2.7,
+            // command_buffer: String::new(),
         }
     }
 }
-
-
-/*
-fn powered_by_egui_and_eframe(ui: &mut egui::Ui) {
-    ui.horizontal(|ui| {
-        ui.spacing_mut().item_spacing.x = 0.0;
-        ui.label("Powered by ");
-        ui.hyperlink_to("egui", "https://github.com/emilk/egui");
-        ui.label(" and ");
-        ui.hyperlink_to(
-            "eframe",
-            "https://github.com/emilk/egui/tree/master/crates/eframe",
-        );
-        ui.label(".");
-    });
-}
-*/
 
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,23 @@
 #![warn(clippy::all, rust_2018_idioms)]
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
+static WINDOW_WIDTH: f32 = 1600.0;
+static WINDOW_HEIGHT: f32 = 900.0;
+static MIN_WINDOW_WIDTH: f32 = 800.0;
+static MIN_WINDOW_HEIGHT: f32 = 600.0;
+
 // When compiling natively:
 #[cfg(not(target_arch = "wasm32"))]
 fn main() -> eframe::Result {
     env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
 
+    let window_res: egui::Vec2 = [WINDOW_WIDTH, WINDOW_HEIGHT].into();
+    let min_window_res: egui::Vec2 = [MIN_WINDOW_WIDTH, MIN_WINDOW_HEIGHT].into();
+
     let native_options = eframe::NativeOptions {
         viewport: egui::ViewportBuilder::default()
-            .with_inner_size([400.0, 300.0])
-            .with_min_inner_size([300.0, 220.0])
+            .with_inner_size(window_res)
+            .with_min_inner_size(min_window_res)
             .with_icon(
                 // NOTE: Adding an icon is optional
                 eframe::icon_data::from_png_bytes(&include_bytes!("../assets/icon-256.png")[..])
@@ -25,51 +33,3 @@ fn main() -> eframe::Result {
     )
 }
 
-/* TODO we do not care for WASM for now
-// When compiling to web using trunk:
-#[cfg(target_arch = "wasm32")]
-fn main() {
-    use eframe::wasm_bindgen::JsCast as _;
-
-    // Redirect `log` message to `console.log` and friends:
-    eframe::WebLogger::init(log::LevelFilter::Debug).ok();
-
-    let web_options = eframe::WebOptions::default();
-
-    wasm_bindgen_futures::spawn_local(async {
-        let document = web_sys::window()
-            .expect("No window")
-            .document()
-            .expect("No document");
-
-        let canvas = document
-            .get_element_by_id("the_canvas_id")
-            .expect("Failed to find the_canvas_id")
-            .dyn_into::<web_sys::HtmlCanvasElement>()
-            .expect("the_canvas_id was not a HtmlCanvasElement");
-
-        let start_result = eframe::WebRunner::new()
-            .start(
-                canvas,
-                web_options,
-                Box::new(|cc| Ok(Box::new(uninterruptible::Uninterruptible::new(cc)))),
-            )
-            .await;
-
-        // Remove the loading text and spinner:
-        if let Some(loading_text) = document.get_element_by_id("loading_text") {
-            match start_result {
-                Ok(_) => {
-                    loading_text.remove();
-                }
-                Err(e) => {
-                    loading_text.set_inner_html(
-                        "<p> The app has crashed. See the developer console for details. </p>",
-                    );
-                    panic!("Failed to start eframe: {e:?}");
-                }
-            }
-        }
-    });
-}
-*/
